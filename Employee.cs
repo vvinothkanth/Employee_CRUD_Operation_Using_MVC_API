822b363eece73ed6b6c72aaaa0494e6c0c103960
vinothkanth <vinothkanth@5gindia.net>
2018-10-22 18:10:10 +0530

    Data

---
+++ Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CURDOperationUsingAjax.Models
+{
+    public class Employee
+    {
+        
+        public int EmployeeID { get; set; }
+
+        
+        public string Name { get; set; }
+
+        
+        public int Age { get; set; }
+
+        
+        public string State { get; set; }
+
+        
+        public string Country { get; set; }
+
+        
+        public string ImageUrl { get; set; }
+
+
+        public HttpPostedFileBase ImageUpload { get; set; }
+
+        public Employee()
+        {
+            ImageUrl = "~/App_Files/Images/ProfilePic.jpg";
+            
+
+        }
+    }
+
+}
---
+++ EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CURDOperationUsingAjax.Models;
+
+namespace CURDOperationUsingAjax.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        EmployeeDBHandler employeeDBHandler = new EmployeeDBHandler();
+        //
+        // GET: /Employee/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult List()
+        {
+            return Json(employeeDBHandler.ListAll(), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Add(Employee emp)
+        {
+            return Json(employeeDBHandler.Add(emp), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetbyID(int ID)
+        {
+            var Employee = employeeDBHandler.ListAll().Find(x => x.EmployeeID.Equals(ID));
+            return Json(Employee, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Update(Employee emp)
+        {
+            return Json(employeeDBHandler.Update(emp), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Delete(int ID)
+        {
+            return Json(employeeDBHandler.Delete(ID), JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
